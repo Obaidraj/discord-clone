3815f2526526a26f222bb8da70cbd628c333335a
iamobaidtariq <iamobaidtariq@gmail.com>
2024-10-21 15:56:14 +0500

    manage members

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,10 +3,12 @@
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "@/components/modals/invite-modal";
 import { EditServerModal } from "@/components/modals/edit-server-modal";
+import { MembersModal } from "@/components/modals/members-modal";
 
 export const ModalProvider = () => {
   return (
     <>
+    <MembersModal/>
       <CreateServerModal />
       <EditServerModal/>
       <InviteModal/>
